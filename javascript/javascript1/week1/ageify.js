20414a7a68c582174f5e2e6ea135567b76f78b58
Alina <73354989+alina-kho@users.noreply.github.com>
2021-12-12 12:48:46 +0100

    Fixing a mistake
    Changed const to let in the formula, so the variables could be modified

--- javascript/javascript1/week1/ageify.js
+++ javascript/javascript1/week1/ageify.js
@@ -1,4 +1,4 @@
 let yearOfBirth = 1997;
 let yearFuture = 2045;
-const age = yearFuture - yearOfBirth;
+let age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture + ".");
